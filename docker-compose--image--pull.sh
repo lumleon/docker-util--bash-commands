315e79b34c6af40fa3cfb52d2e3a66100476a3dd
David Lechner <david.lechner@talsen.team>
2019-05-07 18:50:35 +0200

    add command image--pull

---
+++ docker-compose--image--pull.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source bash-util/functions.sh
+
+prepare_local_environment ${@}
+
+echo -E "Pulling docker images ..."
+
+docker-compose --file ${HOST_PATH_TO_DOCKER_COMPOSE_FILE} \
+               pull
+
+echo -e "Pulling docker images ... $( __done )"
